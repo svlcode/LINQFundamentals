04e25412b4bf8b4819a89d665b38729c693b6928
vio <vio.slanicanu@gmail.com>
2015-06-29 19:28:07 +0300

    added even numbers LINQ query.

--- source/LINQFundamentals/LINQFundamentals/Program.cs
+++ source/LINQFundamentals/LINQFundamentals/Program.cs
@@ -9,6 +9,22 @@ namespace LINQFundamentals
     {
         static void Main(string[] args)
         {
+            ShowEvenNumbers();
+        }
+
+        private static void ShowEvenNumbers()
+        {
+            var numbers = Enumerable.Range(1, 30);
+            var evenNumbers = from number in numbers
+                where number%2 == 0
+                select number;
+
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine("Even number: {0}", number);
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
         }
     }
 }
